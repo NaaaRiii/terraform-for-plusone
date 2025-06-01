864c5d5068f7f868eaeded63ff5568b499e6116d
NaaaRiii <axenarikilo48698062@gmail.com>
2025-06-01 20:53:13 +0900

    fin fix_code0520

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,26 @@
-#data "aws_ami" "amazon_linux2" {
-#  most_recent = true
-#  owners      = ["amazon"] 
+data "aws_ami" "amazon_linux2" {
+  most_recent = true
+  owners      = ["amazon"] 
 
-#  filter {
-#    name   = "name"
-#    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-#  }
-#}
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
 
-#resource "aws_instance" "bastion" {
-#  ami                    = data.aws_ami.amazon_linux2.id
-#  instance_type          = "t3.micro"
-#  subnet_id              = aws_subnet.public_a.id
-#  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.amazon_linux2.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_a.id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
-#  # 公開IPを付与 (SSH接続のため)
-#  associate_public_ip_address = true
+  # 公開IPを付与 (SSH接続のため)
+  associate_public_ip_address = true
 
-#  # 既存のKey Pairを指定 (要作成済)
-#  key_name = "MyKeyPairOne"
+  # 既存のKey Pairを指定 (要作成済)
+  key_name = "MyKeyPairOne"
 
-#  tags = {
-#    Name = "bastion-host"
-#  }
-#}
+  tags = {
+    Name = "bastion-host"
+  }
+}
--- security_groups.tf
+++ security_groups.tf
@@ -86,19 +86,24 @@ resource "aws_security_group" "ecs_task_sg" {
   }
 }
 
-# (B) RDS SG: Bastion SGのみ3306を許可
+########################################################################
+# RDS用のセキュリティグループ
+########################################################################
+
 resource "aws_security_group" "rds_sg" {
   name        = "rds-sg"
   description = "SG for RDS"
   vpc_id      = aws_vpc.main.id
 
-  # 2) ECSタスク SG からの接続を許可 (★追加★)
   ingress {
     description       = "Allow MySQL from ECS Task SG"
     from_port         = 3306
     to_port           = 3306
     protocol          = "tcp"
-    security_groups   = [aws_security_group.ecs_task_sg.id]
+        security_groups = [
+      aws_security_group.ecs_task_sg.id,
+      aws_security_group.bastion_sg.id
+    ]
   }
 
   egress {
@@ -150,5 +155,29 @@ resource "aws_security_group" "alb_sg" {
 
   tags = {
     Name = "alb-sg"
+  }
+}
+
+########################################################################
+#bastion_ec2
+########################################################################
+
+resource "aws_security_group" "bastion_sg" {
+  name   = "bastion-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from office IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["49.98.115.117/32"] //https://www.cman.jp/network/support/go_access.cgi
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
