5b0d815e81b28a593b05f6f7451b6c08bc99ca56
NaaaRiii <axenarikilo48698062@gmail.com>
2025-06-30 12:48:33 +0900

    fin Infrastructure-Configuration

--- ecs_cluster.tf
+++ ecs_cluster.tf
@@ -1,8 +1,6 @@
 resource "aws_ecs_cluster" "main" {
   name = "cluster-for-plusone"
   
-  # capacity_providers = ["FARGATE", "FARGATE_SPOT"] 
-  # オプションとして書いてもOK
   tags = {
     Name = "cluster-for-plusone"
   }
