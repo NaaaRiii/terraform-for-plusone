0151d6baab1acbaafcf2c52320d1e78e1220facf
NaaaRiii <axenarikilo48698062@gmail.com>
2025-07-06 16:32:28 +0900

    fin fix_rails-secrets

--- secrets.tf
+++ secrets.tf
@@ -13,3 +13,11 @@ data "aws_secretsmanager_secret" "db_user" {
 data "aws_secretsmanager_secret_version" "db_user" {
   secret_id = data.aws_secretsmanager_secret.db_user.id
 }
+
+data "aws_secretsmanager_secret" "secret_key_base" {
+  name = "prod/plusone/secret_key_base"
+}
+
+data "aws_secretsmanager_secret_version" "secret_key_base" {
+  secret_id = data.aws_secretsmanager_secret.secret_key_base.id
+}
